f5f8c333678e2de0590c168ada9b10e34d1ae49e
FeCharaf <felipecharaf@gmail.com>
2024-01-31 18:54:41 -0300

    typecast

--- src/aula11.ts
+++ src/aula11.ts
@@ -1,3 +1,13 @@
 let nvalor:number;
 let svalor:string;
 let uvalor:unknown;
+
+uvalor=10;
+nvalor=10;
+svalor=nvalor.toString();
+
+console.log(typeof(svalor));
+console.log(svalor);
+
+nvalor = Number.parseInt(svalor);
+
