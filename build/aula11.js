ddb5ed5288cab4c1dd6e25b5cd540136eb35a2e8
FeCharaf <felipecharaf@gmail.com>
2024-02-01 23:22:02 -0300

    aula12 Functions

--- build/aula11.js
+++ build/aula11.js
@@ -2,3 +2,9 @@
 let nvalor;
 let svalor;
 let uvalor;
+uvalor = 10;
+nvalor = 10;
+svalor = nvalor.toString();
+console.log(typeof (svalor));
+console.log(svalor);
+nvalor = Number.parseInt(svalor);
