afd79ce17431f8f410374bfa951b2b5c74c86ae5
FeCharaf <felipecharaf@gmail.com>
2024-02-02 17:33:02 -0300

    forEach()

--- build/aula14.js
+++ build/aula14.js
@@ -1,1 +1,13 @@
 "use strict";
+const teste = () => {
+    console.log("Teste");
+};
+teste();
+const somaArray = (n) => {
+    let s = 0;
+    n.forEach((element) => {
+        s += element;
+    });
+    return s;
+};
+console.log(somaArray([10, 20, 30]));
--- src/aula14.ts
+++ src/aula14.ts
@@ -1,1 +1,19 @@
 // ARROW FUNCTIONS
+
+const teste=():void=> {
+    console.log("Teste");
+}
+
+teste();
+
+//Criando função de forma direta
+
+const somaArray=(n:number[]):number=>{
+    let s:number=0;
+    n.forEach((element)=>{  // o metodo forEach precisa de uma função com parametro(função de callback)
+        s+=element;
+    })
+    return s;
+}
+
+console.log(somaArray([10, 20, 30]))
