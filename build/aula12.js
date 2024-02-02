c9000b231ec40e60f7269e7d1d3f2f97d8fa7194
FeCharaf <felipecharaf@gmail.com>
2024-02-01 23:30:50 -0300

    part1

--- build/aula12.js
+++ build/aula12.js
@@ -1,1 +1,10 @@
 "use strict";
+function soma(n1, n2) {
+    return n1 + n2;
+}
+function soma2(n1, n2) {
+    let res = n1 + n2;
+    console.log(res);
+}
+soma2(5, 10);
+console.log(soma(2, 5));
--- src/aula12.ts
+++ src/aula12.ts
@@ -1,1 +1,18 @@
 // Functions
+
+
+// Função com dois parametros do tipo number que retorna number.
+function soma(n1:number,n2:number):number {
+    return n1 + n2;
+}
+
+
+
+// Função com dois parametros do tipo number que retorna void.
+function soma2(n1:number, n2:number):void { 
+    let res=n1 + n2;
+    console.log(res);
+}
+
+soma2(5,10);
+console.log(soma(2,5));
