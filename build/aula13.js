fcf645a048ae3130a650569cfcd12d28bcd77f47
FeCharaf <felipecharaf@gmail.com>
2024-02-01 23:41:41 -0300

    parametros opcionais

--- build/aula13.js
+++ build/aula13.js
@@ -3,3 +3,9 @@ function soma(n1 = 0, n2 = 2) {
     return n1 + n2;
 }
 console.log(soma());
+function novoUser(user, pass, nome) {
+    console.log(`User:${user}`);
+    console.log(`Pass::${pass}`);
+    console.log(`Nome::${nome}`);
+}
+novoUser('br', '123');
--- src/aula13.ts
+++ src/aula13.ts
@@ -1,7 +1,19 @@
 // Functions 2
 
-function soma(n1:number=0,n2:number=2):number {  //Valores padroes quando se omite o valor na chamada da função
+
+//Valores padroes quando se omite o valor na chamada da função
+function soma(n1:number=0,n2:number=2):number {  
     return n1 + n2;
 }
 
 console.log(soma())
+
+
+//parametro nome é opcional
+function novoUser(user:string, pass:string, nome?:string):void {
+    console.log(`User:${user}`);
+    console.log(`Pass::${pass}`);
+    console.log(`Nome::${nome}`);
+}
+
+novoUser('br','123');
