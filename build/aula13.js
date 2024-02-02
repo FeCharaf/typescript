0864e1ea5851ec80879d870fe51e6b5a26711d8b
FeCharaf <felipecharaf@gmail.com>
2024-02-01 23:37:07 -0300

    valores padroes

--- build/aula13.js
+++ build/aula13.js
@@ -1,1 +1,5 @@
 "use strict";
+function soma(n1 = 0, n2 = 2) {
+    return n1 + n2;
+}
+console.log(soma());
--- src/aula13.ts
+++ src/aula13.ts
@@ -1,1 +1,7 @@
 // Functions 2
+
+function soma(n1:number=0,n2:number=2):number {  //Valores padroes quando se omite o valor na chamada da função
+    return n1 + n2;
+}
+
+console.log(soma())
