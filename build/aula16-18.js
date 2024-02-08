97995a154c34701d021fe5a68adeb256c442f56c
FeCharaf <felipecharaf@gmail.com>
2024-02-08 18:14:56 -0300

    novos métodos

--- build/aula16-18.js
+++ build/aula16-18.js
@@ -4,11 +4,27 @@ class Computador {
     ram = 0;
     cpu = 0;
     ligado = false;
-    constructor(nome, ram, cpu) {
+    constructor(nome, ram, cpu, ligado = false) {
         this.nome = nome;
         this.ram = ram;
         this.cpu = cpu;
-        console.log(`Novo Computador criado! Nome: ${nome}, Ram: ${ram}, Cpu: ${cpu}`);
+        this.ligado = ligado;
+    }
+    info() {
+        console.log(`Nome..: ${this.nome}`);
+        console.log(`RAM...: ${this.ram}`);
+        console.log(`CPU...: ${this.cpu}`);
+        console.log(`Ligado: ${this.ligado ? "Sim" : "Não"}`);
+        console.log(`---------------------------`);
+    }
+    ligar() {
+        this.ligado = true;
     }
 }
 const comp1 = new Computador("Comp1", 12, 10);
+const comp2 = new Computador("Comp2", 12, 10);
+const comp3 = new Computador("Comp3", 12, 10);
+comp1.ligar();
+comp1.info();
+comp2.info();
+comp3.info();
--- src/aula16-18.ts
+++ src/aula16-18.ts
@@ -1,19 +1,40 @@
 // POO
 
 class Computador {
-    nome:string = "Computador 1";
-    ram:number = 0;
-    cpu:number = 0;
-    ligado:boolean = false;
+  nome: string = "Computador 1";
+  ram: number = 0;
+  cpu: number = 0;
+  ligado: boolean = false;
 
-    constructor(nome:string,ram:number,cpu:number) {
-        this.nome = nome;
-        this.ram = ram;
-        this.cpu = cpu;
-        console.log(`Novo Computador criado! Nome: ${nome}, Ram: ${ram}, Cpu: ${cpu}`)
-    }
+  constructor(nome: string, ram: number, cpu: number, ligado: boolean = false) {
+    // Desligado por padrao
+    this.nome = nome;
+    this.ram = ram;
+    this.cpu = cpu;
+    this.ligado = ligado;
+  }
+
+  info() {
+    // Método info
+    console.log(`Nome..: ${this.nome}`);
+    console.log(`RAM...: ${this.ram}`);
+    console.log(`CPU...: ${this.cpu}`);
+    console.log(`Ligado: ${this.ligado ? "Sim" : "Não"}`); // Operador Ternário
+    console.log(`---------------------------`);
+  }
+
+  ligar() {
+    this.ligado = true;
+  }
 }
 
 // INTANCIAÇÃO
 
-const comp1 = new Computador("Comp1",12,10);
+const comp1 = new Computador("Comp1", 12, 10);
+const comp2 = new Computador("Comp2", 12, 10);
+const comp3 = new Computador("Comp3", 12, 10);
+
+comp1.ligar();
+comp1.info();
+comp2.info();
+comp3.info();
