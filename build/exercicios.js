949464840f678579cbce9c30fc71300674f374e3
FeCharaf <felipecharaf@gmail.com>
2024-03-11 17:17:44 -0300

    exercicios extras

--- build/exercicios.js
+++ build/exercicios.js
@@ -11,4 +11,33 @@ function verificarIdade(idade) {
         return false;
     }
 }
-verificarIdade(19) ? console.log("Sim") : console.log("Não");
+function mostrarDetalhesUsuario(p) {
+    console.log(p.nome);
+    console.log(p.idade);
+}
+const user = { nome: "Felipe", idade: 20 };
+class Animal {
+    nome;
+    tipo;
+    constructor(nome, tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+}
+class Cachorro extends Animal {
+    raca;
+    constructor(raca, nome, tipo) {
+        super(nome, tipo);
+        this.raca = raca;
+    }
+    latir() {
+        console.log("Au Au!");
+    }
+}
+const thor = new Cachorro("border collie", "thor", "grande");
+function inverterArray(array) {
+    return array.reverse();
+}
+const arrayNormal = [1, 2, 3, 4];
+const arrayInverso = inverterArray(arrayNormal);
+console.log(arrayInverso);
--- src/exercicios.ts
+++ src/exercicios.ts
@@ -9,9 +9,58 @@ function verificarIdade(idade: number): boolean {
   } else {
     console.log("É menor de idade");
     return false;
-  }
+}
+
+//verificarIdade(19) ? console.log("Sim") : console.log("Não");
 }
 //3
+interface Usuario {
+    nome:string;
+    idade:number;
+}
 
+function mostrarDetalhesUsuario(p:Usuario) {
+    console.log(p.nome)
+    console.log(p.idade)
+}
 
-verificarIdade(19) ? console.log("Sim") : console.log("Não");
+const user:Usuario = {nome: "Felipe", idade: 20};
+//mostrarDetalhesUsuario(user);
+
+//4
+
+class Animal {
+    nome:string;
+    tipo:string;
+
+    constructor(nome:string,tipo:string) {
+        this.nome = nome
+        this.tipo = tipo
+    }
+}
+
+class Cachorro extends Animal {
+    raca:string
+    constructor(raca:string,nome:string,tipo:string){ // O Construtor do filho precisa de tudo que tem no do pai
+        super(nome,tipo)
+        this.raca = raca;
+    }
+    latir() {
+        console.log("Au Au!")
+    }
+}
+
+const thor = new Cachorro("border collie", "thor", "grande")
+//thor.latir()
+//console.log(thor)
+
+//5
+
+function inverterArray<T>(array: T[]){
+    return array.reverse();
+}
+
+const arrayNormal:number[] = [1, 2, 3, 4]
+const arrayInverso:number[] = inverterArray(arrayNormal);
+console.log(arrayInverso)
+
